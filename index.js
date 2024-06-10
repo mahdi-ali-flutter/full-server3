226f4248120d4abf2d94f9126ce3a7d169e1ffb3
mahdi ali <127436608+mahdi-ali-flutter@users.noreply.github.com>
2024-06-10 14:25:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,139 @@
+
 //
 
 const express=require('express');
+const path=require('path');
 const mongo=require("mongoose");
 const app=express();
 const params=require("params");
 const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true, useNewUrlParser : true }));
-app.use(express.json());
 
-var url = "mongodb://127.0.0.1:27017/notes";
-var url1="mongodb+srv://mahdi:mahdi@cluster0.3lvnvig.mongodb.net/mahdi?retryWrites=true&w=majority";
- var databsae;
+const { resourceLimits } = require('worker_threads');
+app.use(bodyParser.json());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+app.use(bodyParser.json());
+const multer=require('multer');
+app.use(express.static(path.join(__dirname,'images')));
+
+//yUxpIMoLyAUDqc67
+var url1="mongodb+srv://Mahdi:mahdi@cluster0.7v8ud.mongodb.net/mahdi?retryWrites=true&w=majority"
+var url="mongodb+srv://mahdi:mahdi@cluster0.3lvnvig.mongodb.net/mahdi?retryWrites=true&w=majority";
+var databsae;
+var image_url;
 
 mongo.connect(url1,{useNewUrlParser:true,useUnifiedTopology:true},
      function(err, db) {
-    if (err) throw err;
+    if (err) throw err;   
       console.log(" conected database successfuly ...");
-    
-   
-  });
-  var sch={
-    name: String,
-    count: int,
-    price : int
+  });///get item one from page view 
+
+  var product={
+    name :String,
+    price:Number,
+    count:Number,
     details:String,
-    id:Number
-}
-  const mongomodel = new mongo.model('newcols',sch);
-
-
-app.get('/',(req,res)=>{
-   res.send(req.body.email);
-});
-app.get('/data',(req,res,next)=>{
-fetchid=req.params.id  ;
-mongomodel.find(({id:fetchid}),(err,val)=>{
-if(err) throw err;
-res.send(val);
-})
+    image:String
+   }
+   const products=new mongo.model("products",product);
   
-});
+          ////upload iamge 
+
+       const storage= multer.diskStorage({
+            destination:function(req,file,cb){
+              cb(null,path.join(__dirname,"./images"))
+            },
+            filename(req,file,cb){
+              cb(null,newData().replace(/:/g,'_')+file.originalname)
+            }
+          });
+
+     const upload=multer({storage:storage});
+
+
+   app.get('/data',(req,res,next)=>{
+    fetchid=req.params.id  ;
+    products.find(({id:fetchid}),(err,val)=>{
+    if(err) throw err;
+    res.send(val);
+    })});
+  
+  
+    app.post('/post',upload.single('image') ,async(req,res)=>{
+      console.log("inside post function ");
+    
+      const data=new products({
+        name:req.body.name,
+        price:req.body.price,
+        count:req.body.count,
+        decsription:req.body.decsription
+      });
+      const value =await data.save();
+      res.json(value);
+      console.log(req.body);
+    });
+  
+    app.put('/update/:id',async(req,res,next)=>{
+      const ID=req.params.id;
+      const newData={
+        name:req.body.name,
+        price:req.body.price,
+        count:req.body.count,
+        details:req.body.details,
+    }
+    const result=await products.findOneAndReplace({_id:ID}
+      ,newData);
+    console.log(result);
+    res.json({updatedCount : result.modifiedCount});
+    });
+  
+    app.delete('/delete/:id',async(req,res)=>{
+    
+      const ID=req.params.id;
+      const result=await products.deleteOne({_id:ID});
+      res.json({deletedCount:result.deletedCount})
+    
+    });
+
+    app.get('/data/:id',(req,res,next)=>{
+
+      products.findById(req.params.id)
+      .then(
+        (products)=>{
+          if(!products){return res.status(404).send();}
+          res.send(products);
+        }
+      )
+      .catch((err)=>{res.status(500).send(err);});
+    });
+    app.get('/data/:name',(req,res,next)=>{
+          let name1=req.params.name;
+      products.find({name:name1})
+      .then(
+        (products)=>{
+          if(!products){return res.status(404).send();}
+          res.send(products);
+        }
+      )
+      .catch((err)=>{res.status(500).send(err);});
+    });
+
+    app.get('/data1/:name',(req,res,next)=>{
+
+      products.findOne({name:req.params.name})
+      .then(
+        (products)=>{
+          if(!products){return res.status(404).send();}
+          res.send(products);
+        }
+      )
+      .catch((err)=>{res.status(500).send(err);});
+    });
+
+    
+    
+
 
 app.listen(3000,()=>{
     console.log(" conected on port 3000... http://localhost:3000");
-})
+});
